d59b732566a01dd4f69650b02a5e9d247866996e
iknoor12 <imiknoor@gmail.com>
2026-02-03 10:56:53 +0530

    Add files and removes errors

--- random-string_generator/src/App.jsx
+++ random-string_generator/src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useCallback, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // const [randomString, setRandomString] = useState("0");
+  // const [length, setLength] = useState(0);
+
+  // const generateString = useCallback(() => {
+
+  //   const characters =
+  //     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  //   let result = "";
+
+  //   for (let i = 0; i < length; i++) {
+  //     const randomIndex = Math.floor(Math.random() * characters.length);
+  //     result += characters[randomIndex];
+  //   }
+
+  //   setRandomString(result);
+
+  // }, [length]); 
+
+  // useEffect(() => {
+  //   generateString();
+  // }, [generateString]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+
+      <div className="bg-white p-7 rounded-lg shadow-lg w-120 text-center">
+
+        <h1 className="text-2xl font-bold mb-8 text-blue-500">
+          Random String Generator
+        </h1>
+
+        <div className="bg-gray-100 text-lg p-3 rounded mb-6 break-all">
+          {randomString}
+        </div>
+
+        {/* <input
+          type="number"
+          value={length}
+          min="4"
+          max="30"
+          className="border p-3 w-full rounded mb-5"
+          onChange={(e) => setLength(e.target.value)}
+        />
+
+        <button
+          onClick={generateString}
+          className="w-full bg-blue-400 text-white text-lg py-2 rounded hover:bg-blue-600 transition"
+        >
+          Generate New String
+        </button> */}
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    </div>
+  );
 }
 
-export default App
+export default App;
